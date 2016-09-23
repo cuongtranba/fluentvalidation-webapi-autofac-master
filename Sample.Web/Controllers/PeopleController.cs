66fe1480f2f488f20c2383565eb23856316a2c6e
cuongtranba <bacuongtr@gmail.com>
2016-09-23 15:55:05 +0700

    init

--- Sample.Web/Controllers/PeopleController.cs
+++ Sample.Web/Controllers/PeopleController.cs
@@ -8,18 +8,8 @@ namespace Sample.Web.Controllers
 {
     public class PeopleController : ApiController
     {
-        private AbstractValidator<Person> AbstractValidator;
-
-        public PeopleController(AbstractValidator<Person> abstractValidator)
-        {
-            AbstractValidator = abstractValidator;
-        }
-
-
         public IHttpActionResult CreatePerson(Person person)
         {
-            var aa = ErrorMessageResource.MissingUserName;
-            var result = AbstractValidator.Validate(person);
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
